f5d12c40393a3e21142b61161342e62e6cda879a
Debray-Deschodt <debraydeschodtpaul@gmail.com>
2024-03-12 00:26:54 +0100

    certificat

--- environment/production.js
+++ environment/production.js
@@ -2,6 +2,6 @@ const path = require('path')
 
 module.exports = {
     dbUrl: 'mongodb+srv://admin:vz3409aTHKMEZcgO%3F%2FyPCT@uchronie.5j5gecp.mongodb.net/',
-    cert: '/etc/letsencrypt/live/uchronie.ovh/fullchain.pem',
-    key: '/etc/letsencrypt/live/uchronie.ovh/privkey.pem'
+    cert: '/etc/letsencrypt/live/michel-debray-chauvin.ovh/fullchain.pem',
+    key: '/etc/letsencrypt/live/michel-debray-chauvin.ovh/privkey.pem'
 }
